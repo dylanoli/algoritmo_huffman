dad0b3e942f4d57bec2df4364def4b041cb186e4
dylanoli <dylan.oli@hotmail.com>
2019-11-26 16:02:59 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+int makeMenu();
+void compress();
+void decompress();
+
+int main()
+{
+    int option = -1;
+    while (option != 0)
+    {
+        option = makeMenu();
+    }
+    
+    return 0;
+}
+
+int makeMenu()
+{
+    int option = -1;
+    printf("\n---------- Menu Principal ----------");
+    printf("\n[1] Compactar Arquivo");
+    printf("\n[2] Descompactar Arquivo");
+    printf("\n[0] Sair");
+    printf("\n\nSelecione uma opcao: ");
+    scanf("%d",&option);
+    system("clear"); //limpa o console no linux
+    system("cls"); //limpa o console no windows
+
+    switch (option)
+    {
+    case 1:
+        compress();
+        break;
+    case 2:
+        decompress();
+        break;
+    case 0:
+        printf("\nPrograma finalizado.");
+        break;
+    default:
+        printf("\nOpcao invalida, tente novamente.");
+        break;
+    }
+    return option;
+}
+
+void compress()
+{
+    printf("\n---------- Compactar ----------");
+    printf("\nDigite o diretorio do arquivo: ");
+    fflush(stdin);
+    char pathFile[200];
+    scanf("%s",pathFile);
+    // fgets(pathFile, 200,stdin);
+    FILE * pFile = fopen(pathFile,"rb");
+    if (pFile == NULL)
+    {
+        printf("Arquivo nao encontrado !");
+    }
+    else
+    {
+        char c;
+        while (fread(&c,1,1,pFile)>0)
+        {
+            printf("%c",c);
+        }
+    }
+    
+    
+    
+}
+
+void decompress()
+{
+
+}
